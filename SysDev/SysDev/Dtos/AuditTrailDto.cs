a1130232246b26a8abc638aa4130b8dd877b9123
glenx1223 Station1 <glen2mampil.official@gmail.com>
2017-09-13 15:02:17 +0800

    Added Mapping

---
+++ SysDev/SysDev/Dtos/AuditTrailDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using SysDev.Models;
+
+namespace SysDev.Dtos
+{
+    public class AuditTrailDto
+    {
+        public int Id { get; set; }
+        public int ModuleId { get; set; }
+        public int PageId { get; set; }
+        public string Action { get; set; }
+
+        public int UserProfileId { get; set; }
+        public UserProfile UserProfile { get; set; }
+
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
